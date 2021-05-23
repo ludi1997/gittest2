6dc81bbb9e358c6863996d250cbe4f4fb37cfde1
ludi <ludi@xidian.com>
2021-05-23 22:39:07 +0800

    second commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author DiLu
+ * @create 2021-05-23 22:32
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
